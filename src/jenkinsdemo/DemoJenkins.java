94460d797b24884d24e05a8db61bdf48561f40d3
Ravisc <s.c.ravikumar@gmail.com>
2017-03-12 12:54:13 +0530

    hh

---
+++ src/jenkinsdemo/DemoJenkins.java
@@ -0,0 +1,13 @@
+package jenkinsdemo;
+
+import org.testng.annotations.Test;
+
+public class DemoJenkins {
+	
+	@Test
+	public void testjenkins(){
+		
+		System.out.println("Welcome to jenkins world");
+	}
+
+}
